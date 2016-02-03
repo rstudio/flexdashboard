990d10c2b3c4a8fa2029a7723f7e1a4ce86d3717
JJ Allaire <jj@rstudio.org>
2016-02-03 13:41:09 -0500

    initial commit

---
+++ R/keen_dashboard.R
@@ -0,0 +1,50 @@
+
+
+
+#'@export
+keen_dashboard <- function(smart = TRUE,
+                           self_contained = TRUE,
+                           theme = "default",
+                           css = NULL,
+                           includes = NULL,
+                           lib_dir = NULL,
+                           md_extensions = NULL,
+                           pandoc_args = NULL) {
+
+
+  # build pandoc args
+  args <- c("--standalone")
+
+  # use section divs
+  args <- c(args, "--section-divs")
+
+  # additional css
+  for (css_file in css)
+    args <- c(args, "--css", pandoc_path_arg(css_file))
+
+  # add template
+  args <- c(args, "--template",
+            pandoc_path_arg(system.file(package = "dashboards",
+              "rmarkdown/templates/keen_dashboard/resources/default.html")))
+
+  # determine knitr options
+  knitr_options <- knitr_options_html(4, 4, FALSE, FALSE, "png")
+  knitr_options$opts_chunk$echo = FALSE
+
+  # return format
+  output_format(
+    knitr = knitr_options,
+    pandoc = pandoc_options(to = "html",
+                            from = rmarkdown_format(md_extensions),
+                            args = args),
+    keep_md = FALSE,
+    clean_supporting = self_contained,
+    base_format = html_document_base(smart = smart, theme = theme,
+                                     self_contained = self_contained,
+                                     lib_dir = lib_dir, mathjax = NULL,
+                                     template = "default",
+                                     pandoc_args = pandoc_args)
+  )
+
+}
+
---
+++ R/package.R
@@ -0,0 +1,4 @@
+
+
+#' @import rmarkdown
+NULL
