79fb5a5980824fa22e59b2cd2c4f619ba28b9959
JJ Allaire <jj@rstudio.org>
2016-02-04 10:07:18 -0500

    initial work on dashboard.js

--- inst/rmarkdown/templates/keen_dashboard/resources/dashboard.js
+++ inst/rmarkdown/templates/keen_dashboard/resources/dashboard.js
@@ -1,6 +1,33 @@
 
 $(document).ready(function () {
 
+  // find all the level2 sections (those are the rows)
+  var rows = $('div.section.level2');
+  rows.each(function () {
+
+    // remove the h2
+    $(this).children('h2').remove();
+
+    // convert into a bootstrap row
+    $(this).addClass('row');
+
+    // find all of the level 3 subheads
+    var columns = $(this).children('div.section.level3');
+    columns.each(function() {
+
+      // get a reference to the h3 and discover it's inner html
+      var h3 = $(this).children('h3').first();
+      var chartTitle = h3.html();
+
+      // remove the h3
+      h3.remove();
+
+
+
+
+    });
+
+  });
 
 });
 
