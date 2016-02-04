2b4b75c493e5f3a310e4e99a3658c983554ecdef
JJ Allaire <jj@rstudio.org>
2016-02-04 09:38:32 -0500

    add example html structure

--- inst/rmarkdown/templates/keen_dashboard/resources/dashboard.js
+++ inst/rmarkdown/templates/keen_dashboard/resources/dashboard.js
@@ -1,7 +1,25 @@
 
-
-
 $(document).ready(function () {
 
 
 });
+
+
+/*
+<div class="row">
+  <div class="col-sm-6">
+    <div class="chart-wrapper">
+      <div class="chart-title">
+      Pageviews by browser (past 24 hours)
+      </div>
+
+      <div class="chart-stage">
+      </div>
+
+      <div class="chart-notes">
+      This is a sample text region to describe this chart.
+      </div>
+  </div>
+</div>
+
+*/
