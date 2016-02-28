1df90009452344e859e2a9887690b3ee42817642
JJ Allaire <jj@rstudio.org>
2016-02-28 15:59:19 -0500

    correctly handle injection of font-awesome dependency

--- R/flex_dashboard.R
+++ R/flex_dashboard.R
@@ -62,6 +62,7 @@ flex_dashboard <- function(fig_width = 5,
                            theme = "default",
                            highlight = "default",
                            mathjax = "default",
+                           extra_dependencies = NULL,
                            css = NULL,
                            includes = NULL,
                            lib_dir = NULL,
@@ -189,13 +190,9 @@ flex_dashboard <- function(fig_width = 5,
     args
   }
 
-  # font-awesome depencency
-  htmlDeps <- list(htmlDependency(
-    "font-awesome",
-    "4.5.0",
-    src = system.file("www/font-awesome", package = "flexdashboard"),
-    stylesheet = "css/font-awesome.min.css"
-  ))
+  # dependencies
+  extra_dependencies <- append(extra_dependencies,
+                               list(html_dependency_font_awesome()))
 
   # return format
   output_format(
@@ -212,11 +209,20 @@ flex_dashboard <- function(fig_width = 5,
                                      template = "default",
                                      pandoc_args = pandoc_args,
                                      bootstrap_compatible = TRUE,
+                                     extra_dependencies = extra_dependencies,
                                      ...)
   )
-
 }
 
+
+html_dependency_font_awesome <- function() {
+  htmlDependency(
+    "font-awesome",
+    "4.5.0",
+    src = system.file("www/font-awesome", package = "flexdashboard"),
+    stylesheet = "css/font-awesome.min.css"
+  )
+}
 
 pandoc_navbar_args <- function(navbar) {
 
