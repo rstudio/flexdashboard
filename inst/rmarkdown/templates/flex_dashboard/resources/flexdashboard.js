860368a1f05681749768c0550d91e25efe98a3b4
JJ Allaire <jj@rstudio.org>
2016-03-07 09:08:00 -0500

    Revert "don't need a margin-right on the icon element"
    This reverts commit 8acd46213ecffa31f615339d261f400c40904c60.

--- inst/rmarkdown/templates/flex_dashboard/resources/flexdashboard.js
+++ inst/rmarkdown/templates/flex_dashboard/resources/flexdashboard.js
@@ -178,6 +178,8 @@ var FlexDashboard = (function () {
 
       // create the icon
       var iconElement = $('<span class="' + iconSet + ' ' + icon + '"></span>');
+      if (title)
+        iconElement.css('margin-right', '7px');
       iconElement.css('min-width', '20px');
       a.append(iconElement);
       // if url is null see if we can auto-generate based on icon (e.g. social)
