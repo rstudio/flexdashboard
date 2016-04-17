494fa35c001d831f3d69ceadd498d457361d7cdb
dkilfoyle <dkilfoyle@xtra.co.nz>
2016-04-17 12:28:09 +1200

    retrigger justgage animation on tab pane change
    see https://github.com/dkilfoyle/dkjustgage for a standalone widget

--- inst/htmlwidgets/gauge.js
+++ inst/htmlwidgets/gauge.js
@@ -34,8 +34,23 @@ HTMLWidgets.widget({
           config.label = x.label;
 
         // create the justgage if we need to
-        if (justgage === null)
+        if (justgage === null) {
           justgage = new JustGage(config);
+          
+          // retrigger justgage animation when containing tab pane is shown
+          var tab = $(el).closest('div.tab-pane');
+          if (tab !== null) {
+            var tabID = tab.attr('id');
+            var tabAnchor = $('a[data-toggle="tab"][href="#' + tabID + '"]');
+            if (tabAnchor !== null) {
+              tabAnchor.on('shown.bs.tab', function() {
+                justgage.refresh(0, x.max, { refreshAnimationTime: 0, onAnimationEnd: function() {
+                  justgage.refresh(x.value, x.max, { refreshAnimationTime: 700, onAnimationEnd: null });
+                }});
+              });
+            }
+          }
+        }
         else
           justgage.refresh(x.value, x.max);
       },
