fd5d4b65160a595ce78fe22cadb996b7d9403df9
JJ Allaire <jj@rstudio.org>
2016-04-28 05:22:34 -0400

    support all theme colors

--- inst/htmlwidgets/gauge.js
+++ inst/htmlwidgets/gauge.js
@@ -26,7 +26,11 @@ HTMLWidgets.widget({
         }
         for (var i=0; i<x.customSectors.length; i++) {
           var sector = x.customSectors[i];
-          if (sector.color === "success")
+          if (sector.color === "primary")
+            sector.color = themeColor("primary",  "#a9d70b");
+          else if (sector.color === "info")
+            sector.color = themeColor("info",  "#a9d70b");
+          else if (sector.color === "success")
             sector.color = themeColor("success",  "#a9d70b");
           else if (sector.color === "warning")
             sector.color = themeColor("warning",  "#f9c802");
