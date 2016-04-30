c934c9fb3313e847487160c4cc979335783cfb80
JJ Allaire <jj@rstudio.org>
2016-04-30 10:48:35 -0400

    add package-level docs

--- R/package.R
+++ R/package.R
@@ -1,6 +1,26 @@
-
+#' flexdashboard: Interactive dashboards for R
+#'
+#' Create interactive dashboards using \pkg{rmarkdown}.
+#'
+#' @details
+#'
+#' \itemize{
+#' \item{Use R Markdown to publish a group of related data visualizations as a dashboard.}
+#' \item{Ideal for publishing interactive JavaScript visualizations based on htmlwidgets (also works with standard base, lattice, and grid graphics).}
+#' \item{Flexible and easy to specify layouts. Charts are intelligently re-sized to fill the browser and adapted for display on mobile devices.}
+#' \item{Optionally use Shiny to drive visualizations dynamically.}
+#' }
+#'
+#' See the flexdashboard website for additional documentation:
+#' \href{https://rstudio.github.io/flexdashboard}{https://rstudio.github.io/flexdashboard}
+#'
 #' @import rmarkdown
 #' @import htmltools
 #' @importFrom jsonlite toJSON
 #' @importFrom tools file_path_sans_ext
+#'
+#' @docType package
+#' @name flexdashboard
 NULL
+
+
