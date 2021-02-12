b9e9cfb5666a5deef1b976d2e362c352b55c135f
Carson <cpsievert1@gmail.com>
2021-02-12 16:13:58 -0600

    Forgot is_available()

--- R/utils.R
+++ R/utils.R
@@ -42,3 +42,11 @@ accent_colors <- function() {
 dropNulls <- function(x) {
   x[!vapply(x, is.null, FUN.VALUE = logical(1))]
 }
+
+is_available <- function(package, version = NULL) {
+  installed <- nzchar(system.file(package = package))
+  if (is.null(version)) {
+    return(installed)
+  }
+  installed && isTRUE(utils::packageVersion(package) >= version)
+}
