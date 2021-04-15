76c3b70581fcfdb43d16bdd2487a88ef1040e2f7
Carson Sievert <cpsievert1@gmail.com>
2021-04-15 17:23:47 -0500

    Have valueBox() prefer a white contrast (as long it provides a contrast ratio of 2.5 or higher) (#326)

--- tools/updateValueBox.R
+++ tools/updateValueBox.R
@@ -2,10 +2,10 @@ library(sass)
 pkg_dir <- rprojroot::find_package_root_file()
 pkgload::load_all(pkg_dir) # To load themeColors
 
-resources <- file.path(pkg_dir, "inst/rmarkdown/templates/flex_dashboard/resources/")
+flex_home <- file.path(pkg_dir, "inst/www/flex_dashboard")
 
 # Compile CSS the 'core' CSS for viewBox()
-withr::with_dir(resources, {
+withr::with_dir(flex_home, {
   sass(
     sass_file("value-box-sass/core.scss"),
     output = "value-box.css",
@@ -16,7 +16,7 @@ withr::with_dir(resources, {
 # Compile a different CSS for each theme to facilitate viewBox()'s accent colors
 for (theme in names(themeColors)) {
   colors <- themeColors[[theme]]
-  withr::with_dir(resources, {
+  withr::with_dir(flex_home, {
     sass(
       list(
         sprintf(
