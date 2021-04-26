70de2634c93529f9aff28ba4ea54e0f2223b6021
Carson <cpsievert1@gmail.com>
2021-04-26 16:58:25 -0500

    Follow up to #327: properly set bootswatch: cosmo default and warn if relevant for !expr usage

--- R/bslib.R
+++ R/bslib.R
@@ -8,14 +8,24 @@ resolve_theme <- function(theme) {
   if (is.character(theme)) {
     return(theme)
   }
+
   if (is.list(theme)) {
-    if (!is_bs_theme(theme)) {
-      theme <- do.call(bs_theme, theme)
+    # Flexdashboard wants to default to bs_theme(bootswatch = 'cosmo'), but we
+    # can't do that if theme is already a bs_theme() object since updating the
+    # bootswatch theme will override the user's variable defaults
+    if (is_bs_theme(theme) && is.null(theme_bootswatch(theme))) {
+      warning(
+        "Consider adding `bootswatch = 'cosmo'` and `'enable-rounded' = TRUE` ",
+        "to `bs_theme()` to get the same styling defaults that you get without ",
+        "using `!expr` inside `theme:`.",
+        call. = FALSE
+      )
     }
     # Default to cosmo theme (just like the non-bslib usage does)
     # (Users can explictly opt-out with bootswatch: default)
-    if (is.null(theme_bootswatch(theme))) {
-      theme <- bs_theme_update(theme, bootswatch = "cosmo")
+    if (!is_bs_theme(theme)) {
+      theme <- utils::modifyList(list(bootswatch = "cosmo"), theme)
+      theme <- do.call(bs_theme, theme)
     }
     # Also default to enable-rounded: true
     if (!grepl("^\\s*\\$enable-rounded:", sass::as_sass(theme))) {
