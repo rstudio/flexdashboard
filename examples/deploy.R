8b4ff8b73f5daf8a0d0da71ee50f3739efd3f088
Carson <cpsievert1@gmail.com>
2023-01-10 13:04:27 -0600

    Deploy example apps to testing-apps.shiny.io

---
+++ examples/deploy.R
@@ -0,0 +1,17 @@
+library(rsconnect)
+
+example_dir <- rprojroot::find_package_root_file("examples")
+app_basename <- grep("^[0-9][0-9]_", list.dirs(example_dir, full.names = FALSE), value = TRUE)
+app_name <- vapply(strsplit(apps, "_"), function(x) x[[2]], character(1))
+
+
+Map(
+  f = function(name, basename) {
+    file <- file.path(example_dir, basename, "dashboard.Rmd")
+    if (!file.exists(file)) return()
+
+    message("---- Deploying flexdashboard example app ", name, " --------")
+
+    deployApp(appDir = file, appName = paste0("flexdashboard-", name), account = "testing-apps")
+  }, app_name, app_basename
+)
